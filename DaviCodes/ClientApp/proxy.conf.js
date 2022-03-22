53322714eed6064429a8f1eccccff679deebdf1a
Davivi <48662592+d-magit@users.noreply.github.com>
2022-03-21 22:05:41 -0300

    Changing proxying and port settings

--- DaviCodes/ClientApp/proxy.conf.js
+++ DaviCodes/ClientApp/proxy.conf.js
@@ -1,7 +1,6 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:59002';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` : env.ASPNETCORE_URLS;
 
 const PROXY_CONFIG = [
   {
